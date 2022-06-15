ef8b46a26e6cd8e5782ac8f117e3681b9bddf7cc
Daniil Schmelzer <daniilschmelzer@gmail.com>
2022-06-15 15:53:48 +0300

    initial commit

---
+++ src/main.java
@@ -0,0 +1,14 @@
+public class main {
+    public static void main(String[] args) {
+        int currentSumm = 145;
+        int depositAmount = 2200;
+        int bonus;
+        int finalAmount;
+        if (depositAmount > 1000) {
+            finalAmount = currentSumm + depositAmount / 100 + depositAmount;
+        } else {
+            finalAmount = currentSumm + depositAmount;
+        }
+        System.out.println(finalAmount);
+    }
+}
